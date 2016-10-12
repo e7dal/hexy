fef561836a887babad0d5cfcd03da82fb44a748e
Erdal Yildiz <eenheellangemailadres@gmail.com>
2016-10-12 21:28:14 +0200

    WIP, made the first runnable example test case

--- hexy/commands/cmd_examples.py
+++ hexy/commands/cmd_examples.py
@@ -20,12 +20,12 @@ from ..util.examples import EXAMPLES as examples
 @pass_hexy
 def cli(ctx, name,all):
     """Show example for doing some task in hexy"""
-    ctx.gbc.say('examples',stuff=examples, verbosity=1000)
+    ctx.say('examples',stuff=examples, verbosity=1000)
 
     for example in examples:
         if all or (name and example['name'] == name):
             if all:
-                ctx.gbc.say('example',stuff=example, verbosity=100)
+                ctx.say('example',stuff=example, verbosity=100)
                 name = example['name']
             #click.echo_via_pager(example['fun']())
             click.echo("#"*80)
@@ -36,8 +36,7 @@ def cli(ctx, name,all):
             click.echo("### end of hexy example: "+name)
             click.echo("#"*80)
             click.echo()
-
-        else:
+        if not all and not name:
             click.echo("available example: " + example['name'])
 
 
--- hexy/util/examples.py
+++ hexy/util/examples.py
@@ -7,11 +7,119 @@ def example(fun):
  EXAMPLES.append({'name':fun.__name__,'fun':fun})
  return fun
 
+#(:. runnable examples for biotick testing  .:)
+#todo heredocs working: http://www.tldp.org/LDP/abs/html/here-docs.html
+
 @example
 def get_example_cmd_start():
- """an example:stsrting"""
+ """an example:starting"""
  return  """# starting
-$ hexy
+# $ hexy 
+
+hexy  > example_cmd_start.out         #todo STDERR
+#should show:
+cat << "END_OF_EXAMPLE"  > expect_example_cmd_start.out
+Usage: hexy [OPTIONS] COMMAND [ARGS]...
+
+  hexy, hexagonal ascii drawing
+
+Options:
+  -c, --config KEY VALUE    overrides a config key/value pair.
+  -v, --verbose INTEGER     sets verbose, bigger is more detail
+  -b, --barverbose INTEGER  sets a bar, only show messages up to the bar
+  -p, --profile             run hexy with profiling.
+  --version                 Show the version and exit.
+  --help                    Show this message and exit.
+
+Commands:
+  draw      Draw and show grid in hexy
+  examples  Show example for doing some task in hexy
+  grid      Show empty grid in hexy
+  manual    Shows the man page packed inside the hexy tool
+  read      Read ascii grid into an hexy grid
+END_OF_EXAMPLE
+
+#check  and test example like:
+# $ hexy exmamples -n example_cmd_start|bash
+
+#the biot test is just a simple diff:
+res=(diff expect_example_cmd_start.out  example_cmd_start.out)
+ret=$?
+if [ $ret -eq 0 ]; then
+    echo "OK: example_cmd_start"
+fi
+if [ $ret -ne 0 ]; then
+    echo $res
+    echo "NO: example_cmd_start"
+fi
 """
 
+@example
+def get_example_cmd_grid_default():
+ """an example: hexy grid, with default values"""
+ return  """# starting
+$ hexy grid
+
+should show:
+
+:|1 2 3 4 5 6 7 |:
+:|______________|:
+:|. . . . . . . |:1
+:| . . . . . . .|:2
+:|. . . . . . . |:3
+:| . . . . . . .|:4
+:|______________|:
+:| 1 2 3 4 5 6 7|:
+
+"""
+
+@example
+def get_example_cmd_draw_default():
+ """an example: hexy grid, with default values"""
+ return  """# starting
+$ hexy draw
+
+should show:
+
+:|1 2 3 4 5 6 7 8 9 0 |:
+:|____________________|:
+:|. . . . . . . . . . |:1
+:| . . . . . . . . . .|:2
+:|. . _ _ _ . . . . . |:3
+:| . / . . \ . . . . .|:4
+:|. / . . . \ . . . . |:5
+:| .\. . . ./. . . . .|:6
+:|. .\_ _ _/. . . . . |:7
+:| . . . . . . . . . .|:8
+:|. . . . . . . . . . |:9
+:| . . . . . . . . . .|:10
+:|____________________|:
+:| 1 2 3 4 5 6 7 8 9 0|:
+
+"""
+
+@example
+def get_example_cmd_read_default():
+ """an example: hexy grid, with default values"""
+ return  """# starting
+$ hexy draw|hexy read
+
+should show:
+
+:|1 2 3 4 5 6 7 8 9 0 |:
+:|____________________|:
+:|. . . . . . . . . . |:1
+:| . . . . . . . . . .|:2
+:|. . _ _ _ . . . . . |:3
+:| . / . . \ . . . . .|:4
+:|. / . . . \ . . . . |:5
+:| .\. . . ./. . . . .|:6
+:|. .\_ _ _/. . . . . |:7
+:| . . . . . . . . . .|:8
+:|. . . . . . . . . . |:9
+:| . . . . . . . . . .|:10
+:|____________________|:
+:| 1 2 3 4 5 6 7 8 9 0|:
+
+"""
 # vim: tabstop=1 expandtab shiftwidth=1 softtabstop=1
