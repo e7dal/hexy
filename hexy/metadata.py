d74b81b1ec0ea679511ead272f30e0b7fdb88bd0
Erdal Yildiz <eenheellangemailadres@gmail.com>
2016-10-25 23:28:06 +0200

    update version

--- hexy/metadata.py
+++ hexy/metadata.py
@@ -10,7 +10,7 @@ Information describing the project.
 package = 'hexy'
 project = 'cli tool to create hexagonal ascii based drawings'
 
-version = "2016.10.12"
+version = "2016.10.25"
 
 description =project
 authors = ['Erdal Yildiz']
