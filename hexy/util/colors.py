753ec6e7785634d05633251a41baab037a8519e6
rdl <eenheellangemailadres@gmail.com>
2016-11-06 15:41:11 +0100

    added cycling color

--- hexy/util/colors.py
+++ hexy/util/colors.py
@@ -4,24 +4,16 @@ from colored import fg,bg,attr
 #from m import m
 #from p import p
 #from r import r
-def c(i,m):
+def color(i,m):
  i=i%255
  return '%s%s%s'%(fg(i),m,attr(0))
 
-def b(i,m):
+def bcolor(i,m):
  i=i%255
  return '%s%s%s'%(bg(i),m,attr(0))
 
-def D():
- s=''
- x='colors available'
- l=len(x)
- for i in range(1,l+1):
-  s+=c(i*10,x[i-1])
- print(s) 
-
-#a(c(65,'colors available'))
-for i in range(0,256,1):
-  print(c(i,str(i)))
-  print(b(i,str(i)))
-
+CURRCOLOR=0
+def cycling_color(c):
+ global CURRCOLOR
+ CURRCOLOR+=1
+ return color(CURRCOLOR,c)
