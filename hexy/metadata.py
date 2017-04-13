b4f9d92804beaab7d260d417cf5cc412124478e3
No Name <nomail@nomail.nomail>
2017-04-13 22:29:18 +0200

    new version

--- hexy/metadata.py
+++ hexy/metadata.py
@@ -10,7 +10,7 @@ Information describing the project.
 package = 'hexy'
 project = 'cli tool to create hexagonal ascii based drawings'
 
-version = "2016.10.25"
+version = "2017.04.13"
 
 description =project
 authors = ['Erdal Yildiz']
