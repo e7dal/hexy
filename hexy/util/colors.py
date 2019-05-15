8fba0ecb8b34c1100ea53dd153622effbe261ee4
Erdal Yildiz <ikwiltotaalgeenspamemailontvangen@ikwiltotaalgeenspamemailontvangen.bio>
2019-05-15 21:25:29 +0200

    bg/fg coloring

--- hexy/util/colors.py
+++ hexy/util/colors.py
@@ -1,19 +1,64 @@
 from colored import fg,bg,attr
-#from a import a
-#from d import d
-#from m import m
-#from p import p
-#from r import r
+
 def color(i,m):
- i=i%255
  return '%s%s%s'%(fg(i),m,attr(0))
 
 def bcolor(i,m):
- i=i%255
  return '%s%s%s'%(bg(i),m,attr(0))
 
-CURRCOLOR=0
-def cycling_color(c):
+
+#def fib(i=0):
+# if i == 0: yield 1
+# if i == 1: yield 1
+# if i >1 and i < 100: yield fib(i-1)+fib(i-2)
+F=[0,1]
+def fib(n=1):
+ #print(n,len(F))
+ if len(F) >= n:
+  return F[n-1]
+ for i in range(len(F),n):
+  F.append(F[-1]+F[-2])
+ return F[n-1]
+
+
+def fibd(n=1):
+ if n == 1: return 0
+ if n == 2: return 1
+ P1=0
+ P2=1
+ for i in range(3,n):
+  T=P1+P2
+  P2=P1
+  P1=T
+ return T
+
+C={ 'CURRCOLOR':0,
+    'BGCURRCOLOR':127,
+    'COUNT':0,
+    'FN':3,
+    'FFG':1000,
+    'FBG':2000}
+
+def cycling_color(c,bg=True):
+ """
+ global FN
  global CURRCOLOR
- CURRCOLOR+=1
- return color(CURRCOLOR,c)
+ global FFC
+ global BGCURRCOLOR
+ global FBC
+ global COUNT
+ COUNT+=1
+ """
+ if C['COUNT']%C["FFG"] == 0:
+  C["CURRCOLOR"]+=1
+  C["CURRCOLOR"]%=255
+  C["FFG"]=fib(C["FN"])
+  C["FN"]+=1
+ if C["COUNT"]%C["FBG"] == 0:
+  C["BGCURRCOLOR"]+=1
+  C["BGCURRCOLOR"]%=255
+  C["FBG"]=fib(C["FN"])
+  C["FN"]+=1
+ if bg:
+  return bcolor(C["BGCURRCOLOR"],color(C["CURRCOLOR"],c))
+ return  color(C["CURRCOLOR"],c)
